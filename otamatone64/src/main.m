165439d00634e494a72e8ed7e435da847daf36e3
n96j <mateuslucas230579@gmail.com>
2026-08-14 20:36:35 -0300

    Add ARM64 iOS entry point

---
+++ otamatone64/src/main.m
@@ -0,0 +1,3 @@
+#import <UIKit/UIKit.h>
+#import "GameProjectAppDelegate.h"
+int main(int argc,char*argv[]){@autoreleasepool{return UIApplicationMain(argc,argv,nil,NSStringFromClass(GameProjectAppDelegate.class));}}
